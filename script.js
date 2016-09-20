34afa9bd19bb99a4946df9a5952693a81b43de8b
Alan Cruse <alancruse86@gmail.com>
2016-09-20 21:55:18 +0200

    added mouse draw and grid resizing

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-	var div=$('<div class="grid"></div>')
-	var count = 0
-	var input = 16
-	var squared = input * input
-	while (count < squared - 1) {
-		$(div).last().clone().appendTo('#wrapper');
+
+	$('button').click(function() {
+		$('.grid').removeClass("black");
+		$('#wrapper').empty();
+		var div=$('<div class="grid"></div>');
+		var input = prompt('Please select number of squares');
+		var size = (700 / input) - 2
 		
-		count++;
-	};
-	
+
+		var count = 0;
+		var squared = input * input;
+		while (count < squared) {
+			
+			$(div).last().clone().appendTo('#wrapper');
+			count++;
+		};
+		$('.grid').height(size).width(size);
+
 	$('.grid').hover(function() {
-	$(this).addClass("black");
-});
-
+		$(this).addClass("black");
 
 });
+	});
+
+	
 
 
+
+});
+
+		
