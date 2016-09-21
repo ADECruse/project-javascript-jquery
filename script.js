88722d5140184f04a770d73b78f7e06ecabf96df
Alan Cruse <alancruse86@gmail.com>
2016-09-21 20:51:42 +0200

    Added rainbow option and changed css layout

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 		var div=$('<div class="grid"></div>');
 		var input = Number(window.prompt('Please enter grid size','32'));
-		var size = (700 / input) - 2
+		var size = Math.floor(960 / input) - 2;
 		var count = 0;
 		var squared = input * input;
 		while (count < squared) {
@@ -18,7 +18,9 @@ $(document).ready(function() {
 		$('.grid').height(size).width(size);
 
 		$('.grid').hover(function() {
+			
 			$(this).addClass("black");
+
 			});
 		});
 		
@@ -35,10 +37,11 @@ $(document).ready(function() {
 
 			var div=$('<div class="grid"></div>');
 			var input = Number(window.prompt('Please enter grid size','32'));
-			var size = (700 / input) - 2
+			var size = (960 / input) - 2
 			var count = 0;
 			var squared = input * input;
 			while (count < squared) {
+
 				$(div).last().clone().appendTo('#wrapper');
 				count++;
 			};
@@ -55,10 +58,38 @@ $(document).ready(function() {
 			});  
 		});
 
+
+		$('#rainbow').click(function() {
+		$('.grid').removeClass("black");
+		$('#wrapper').empty().removeClass("black");
+
+		var div=$('<div class="grid"></div>');
+		var input = Number(window.prompt('Please enter grid size','32'));
+		var size = (960 / input) - 2
+		var count = 0;
+		var squared = input * input;
+		while (count < squared) {
+			
+			$(div).last().clone().appendTo('#wrapper');
+			count++;
+		};
+		
+		$('.grid').height(size).width(size);
+
+		$('.grid').hover(function() {
+			function random_color() {
+    			return '#'+Math.floor(Math.random()*16777215).toString(16);
+			}
+			$(this).css("background-color", random_color);
+
+			});
+		});
+
 	
 
 
 
 });
+
 
 		
