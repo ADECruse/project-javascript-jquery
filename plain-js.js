5d965fd44f6f8002523680e2a13e350abe15e4f0
Alan D. E. Cruse <alancruse@tutanota.com>
2020-05-19 13:17:57 +0200

    Add rainbow mode functionality

--- plain-js.js
+++ plain-js.js
@@ -1,4 +1,6 @@
 const classicButton = document.getElementById('classic');
+const gradientButton = document.getElementById('gradient');
+const rainbowButton = document.getElementById('rainbow');
 const grid = document.getElementById('grid');
 
 const addBlack = function (e) {
@@ -7,11 +9,18 @@ const addBlack = function (e) {
 }
 
 const addGradientBlack = function (e) {
-    
+    let gradient = this.style.opacity;
+    this.style.opacity = "-=0.1";
+    console.log("Gradient" + gradient);
+	if (gradient > 0) {
+		
+        console.log("Gradient" + gradient);
+	};
 }
 
 const addRainbow = function (e) {
-    
+    let randomColour = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.style.backgroundColor = randomColour;
 }
 
 const resetGrid = function (e) {
@@ -51,5 +60,23 @@ const newClassicGrid = function (e) {
     cell.forEach(e => e.addEventListener('mouseover', addBlack));
 }
 
+const newGradientGrid = function (e) {
+    resetGrid();
+    createGrid();
+    grid.style.backgroundColor = 'white';
+    const cell = document.querySelectorAll('.cell');
+    cell.forEach(e => e.addEventListener('mouseover', addRainbow));
+}
+
+const newRainbowGrid = function (e) {
+    resetGrid();
+    createGrid();
+    const cell = document.querySelectorAll('.cell');
+    cell.forEach(e => e.addEventListener('mouseover', addRainbow));
+}
+
 classicButton.addEventListener('click', newClassicGrid);
+gradientButton.addEventListener('click', newGradientGrid);
+rainbowButton.addEventListener('click', newRainbowGrid);
+
 // cell.forEach(e => e.addEventListener('hover', addBlack))
